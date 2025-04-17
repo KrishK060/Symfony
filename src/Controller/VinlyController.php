ffff1b8f61c831267ff9a6ee138521d2096763a4
Krishk060 <krishkalaria0601@gmail.com>
2025-04-17 16:30:49 +0530

    created a twign

--- src/Controller/VinlyController.php
+++ src/Controller/VinlyController.php
@@ -2,15 +2,24 @@
 
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use function Symfony\Component\String\u;
 
-class VinlyController
+class VinlyController extends AbstractController
 {
     #[Route('/')]
     public function homepage(){
-        return new Response('hello symfony');
+        // return new Response('hello symfony');
+        $tracks = [
+            ['song'=>'gangstar paradice','artist'=>'crazy'],
+            ['song'=>'waterfall','artist'=>'abc']
+        ];
+        return $this->render('vinly/homepage.html.twig',[
+            'title'=>'songs',
+            'tracks'=>$tracks
+        ]);
     }
 
     #[Route('/browse/{slug}')]
